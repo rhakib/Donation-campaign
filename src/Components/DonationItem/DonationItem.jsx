27f224fc8cbd45960a53f2774f0ba38bbabb6f0a
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2023-09-25 21:49:34 +0600

    donation cards are ready

--- src/Components/DonationItem/DonationItem.jsx
+++ src/Components/DonationItem/DonationItem.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const DonationItem = ({card}) => {
-    const {id, picture, title, category, category_bg, card_bg, text_button_bg, description, price} = card;
+const DonationItem = ({ card }) => {
+    const { id, picture, title, category, category_bg, card_bg, text_button_bg, description, price } = card;
 
     return (
-        
-        <div>
-            
-        </div>
+
+        <Link> 
+            <div className='pb-5 rounded-lg space-y-4' style={{ backgroundColor: card_bg }}>
+                <img className='w-full' src={picture} alt="" />
+                <div className='pl-4'>
+                    <p className='w-24 text-center p-1 px-2 rounded-md' style={{ backgroundColor: category_bg, color: text_button_bg }}>{category}</p>
+                    <h3 className='font-bold text-xl' style={{ color: text_button_bg }}>{title}</h3>
+                </div>
+            </div>
+        </Link>
     );
 };
 
--- src/Components/DonationList/DonationList.jsx
+++ src/Components/DonationList/DonationList.jsx
@@ -5,7 +5,7 @@ const DonationList = ({donationList}) => {
     console.log(donationList);
     return (
         <div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-12'>
                 {
                     donationList?.map(card => <DonationItem key={card.id} card={card}></DonationItem>)
                 }
